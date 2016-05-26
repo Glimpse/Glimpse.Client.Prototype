52ae85811c90e7aaf274497539521ed04f860fb8
Phillip M. Hoff <phillip@orst.edu>
2016-05-26 15:35:30 -0700

    Added unit tests for selectors.

--- src/request/actions/RequestDetailDataActions.ts
+++ src/request/actions/RequestDetailDataActions.ts
@@ -2,8 +2,4 @@ import { createActionCreator } from './ActionCreator';
 
 import { Action } from 'redux';
 
-export interface IRequestDetailDataSelectOperationAction extends Action {
-    selectedIndex: number;
-}
-
-export const selectOperationAction = createActionCreator<number>('request.detail.data.selectOperation');
+export const selectOperationAction = createActionCreator<number>('request.detail.data.selected');
