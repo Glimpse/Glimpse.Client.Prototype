37bbd7952177d0a7f3c0b3b1a32dd972f38f8bb2
Phillip M. Hoff <phillip@orst.edu>
2016-06-08 08:53:27 -0700

    Switch web services tab to use common tri-panel.

--- src/request/components/RequestDetailPanelWebServices.tsx
+++ src/request/components/RequestDetailPanelWebServices.tsx
@@ -1,4 +1,5 @@
 import { IRequestDetailWebServicesRequestState } from '../stores/IRequestDetailWebServicesRequestState';
+import { MasterDetailTriPanel } from './MasterDetailTriPanel';
 
 import _ = require('lodash');
 import React = require('react');
@@ -36,7 +37,7 @@ class ServiceMessages extends React.Component<IServiceMessagesProps, {}> {
         }
         
         return (
-            <table className='table table-bordered table-striped tab-content-item table-selectable'>
+            <table className='table table-bordered table-striped table-selectable tab-web-services-table'>
                 <thead>
                     <tr className='table-col-title-group'>
                         <th><span className='table-col-title'>Name/Path</span></th>
@@ -49,11 +50,6 @@ class ServiceMessages extends React.Component<IServiceMessagesProps, {}> {
                     </tr>
                 </thead>
                 {requestItems}
-                <tfoot>
-                    <tr className='table-body-padding table-col-title-group'>
-                        <th colSpan={7}></th>
-                    </tr>
-                </tfoot>
             </table>
         );
     }
@@ -80,35 +76,6 @@ class ServiceDetailsHeaders extends React.Component<{ headers }, {}> {
     }
 }
 
-interface IServiceDetailsProps {
-    requestHeaders: { [key: string]: string };
-    responseHeaders: { [key: string]: string };
-}
-
-/**
- * React class to display service details
- */
-class ServiceDetails extends React.Component<IServiceDetailsProps, {}> {
-    public render() {
-        return (
-            <table className="table table-details">
-                <thead>
-                    <tr>
-                        <th className="table-col-title" width="50%">Request</th>
-                        <th className="table-col-title" width="50%">Response</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td><ServiceDetailsHeaders headers={this.props.requestHeaders} /></td>
-                        <td><ServiceDetailsHeaders headers={this.props.responseHeaders} /></td>
-                    </tr>
-                </tbody>
-            </table>
-        );
-    }
-}
-
 export interface IWebServicesProps {
     requests: IRequestDetailWebServicesRequestState[];
     selectedRequest: IRequestDetailWebServicesRequestState;
@@ -123,25 +90,37 @@ interface IWebServicesCombinedProps extends IWebServicesProps, IWebServicesDispa
 
 export class WebServices extends React.Component<IWebServicesCombinedProps, {}> {
     public render() {
-        if (this.props.requests.length > 0) {
-            const selectedRequestId = this.props.selectedRequest ? this.props.selectedRequest.id : '';
-            const requestHeaders: { [key:string]: string } = this.props.selectedRequest ? this.props.selectedRequest.requestHeaders : {};
-            const responseHeaders: { [key:string]: string } = this.props.selectedRequest ? this.props.selectedRequest.responseHeaders : {};
+        return (
+            <div className='tab-web-services'>
+            {
+                this.props.requests.length > 0
+                    ? <MasterDetailTriPanel masterPanel={this.renderMaster()} leftDetailPanel={this.renderLeftDetail()} leftDetailPanelTitle='Request' rightDetailPanel={this.renderRightDetail()} rightDetailPanelTitle='Response' />
+                    : <div className="tab-section text-minor">Could not find any data.</div>
+            }
+            </div>
+        );
+    }
 
-            return (
-                <div className="tab-content tab-detail-holder"> 
-                    <div className="tab-detail-body">
-                        <h3>{this.props.requests.length} Requests</h3>
-                        <ServiceMessages onSelectRequest={requestId => this.props.onSelectRequest(requestId)} selectedRequestId={selectedRequestId} requests={this.props.requests} />
-                    </div>
-                    <div className="tab-detail-footer">
-                        <ServiceDetails requestHeaders={requestHeaders} responseHeaders={responseHeaders} />
-                    </div> 
-                </div>
-            );
-        }
-        else {
-            return <div className="tab-section text-minor">Could not find any data.</div>;
-        }
+    private renderMaster() {
+        const selectedRequestId = this.props.selectedRequest ? this.props.selectedRequest.id : '';
+        
+        return (
+            <div className="tab-web-services-master">
+                <h3>{this.props.requests.length} Requests</h3>
+                <ServiceMessages onSelectRequest={requestId => this.props.onSelectRequest(requestId)} selectedRequestId={selectedRequestId} requests={this.props.requests} />
+            </div>
+        );
+    }
+
+    private renderLeftDetail() {
+        const requestHeaders: { [key:string]: string } = this.props.selectedRequest ? this.props.selectedRequest.requestHeaders : {};
+        
+        return <ServiceDetailsHeaders headers={requestHeaders} />;
+    }
+
+    private renderRightDetail() {
+        const responseHeaders: { [key:string]: string } = this.props.selectedRequest ? this.props.selectedRequest.responseHeaders : {};
+        
+        return <ServiceDetailsHeaders headers={responseHeaders} />;
     }
 }
