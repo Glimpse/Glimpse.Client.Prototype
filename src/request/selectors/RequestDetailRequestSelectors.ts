d2deb7e5227bd22fdb1c7acb1c78acd25bda7370
Phillip M. Hoff <phillip@orst.edu>
2016-06-14 14:17:04 -0700

    Shows all middleware in a "flattened" list.

--- src/request/selectors/RequestDetailRequestSelectors.ts
+++ src/request/selectors/RequestDetailRequestSelectors.ts
@@ -1,3 +1,34 @@
 import { IRequestState } from '../stores/IRequestState';
 
-export const getRequest = (state: IRequestState) => state.detail.request;
+import { createSelector } from 'reselect';
+
+const getRequestState = (state: IRequestState) => state.detail.request;
+
+function flattenMiddlewareRecursive(middleware, middlewareArray, depth: number) {
+
+    middleware.forEach(middlewareItem => {
+        middlewareArray.push(middlewareItem);
+
+        flattenMiddlewareRecursive(middlewareItem.middleware, middlewareArray, depth + 1);
+    });
+}
+
+function flattenMiddleware(middleware) {
+    const middlewareArray = [];
+
+    flattenMiddlewareRecursive(middleware, middlewareArray, 0);
+
+    return middlewareArray;
+}
+
+export const getRequest = createSelector(
+    getRequestState,
+    requestState => {
+        return {
+            url: requestState.url,
+            middleware: flattenMiddleware(requestState.middleware),
+            request: requestState.request,
+            response: requestState.response
+        }
+    }
+);
