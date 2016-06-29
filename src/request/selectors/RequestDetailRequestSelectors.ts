c00878a30bb7e98c01acf906b41f0dbec7a9ba0d
Phillip M. Hoff <phillip@orst.edu>
2016-06-29 08:47:53 -0700

    Basic strikethrough of non-current headers.

--- src/request/selectors/RequestDetailRequestSelectors.ts
+++ src/request/selectors/RequestDetailRequestSelectors.ts
@@ -10,31 +10,49 @@ const getMiddlewareState = (state: IRequestState) => state.detail.request.middle
 export const getRequest = (state: IRequestState) => state.detail.request;
 export const getUrl = (state: IRequestState) => state.detail.request.url;
 
-interface IFlattenedMiddleware {
-    depth: number,
-    middleware: { name: string, packageName: string, headers: { [key: string]: { value: string, isCurrent: boolean } } }
+interface IMiddleware { 
+    name: string, 
+    packageName: string, 
+    headers: { [key: string]: { value: string, isCurrent: boolean } } 
 }
 
-function flattenMiddlewareRecursive(middleware: IRequestDetailRequestMiddlewareState[], middlewareArray: IFlattenedMiddleware[], depth: number): void {
+
+interface IFlattenedMiddleware {
+    depth: number,
+    middleware: IMiddleware
+}
+
+function flattenMiddlewareRecursive(middleware: IRequestDetailRequestMiddlewareState[], middlewareArray: IFlattenedMiddleware[], currentHeaders: { [key: string]: IMiddleware }, depth: number): void {
 
     middleware.forEach(middlewareItem => {
-        middlewareArray.push({
-            depth: depth,
-            middleware: {
-                name: middlewareItem.name,
-                packageName: middlewareItem.packageName,
-                headers: _.mapValues(middlewareItem.headers, value => { return { value: value, isCurrent: false }; })
-            }
+        const middleware = {
+            name: middlewareItem.name,
+            packageName: middlewareItem.packageName,
+            headers: _.mapValues(middlewareItem.headers, value => { return { value: value, isCurrent: false }; })
+        };
+
+        _.forEach(middleware.headers, (value, key) => {
+            currentHeaders[key] = middleware;
         });
 
-        flattenMiddlewareRecursive(middlewareItem.middleware, middlewareArray, depth + 1);
+        middlewareArray.push({
+            depth: depth,
+            middleware: middleware
+        });
+
+        flattenMiddlewareRecursive(middlewareItem.middleware, middlewareArray, currentHeaders, depth + 1);
     });
 }
 
 function flattenMiddleware(middleware: IRequestDetailRequestMiddlewareState[]): IFlattenedMiddleware[] {
     const middlewareArray = [];
+    const currentHeaders: { [key: string]: IMiddleware } = {};
 
-    flattenMiddlewareRecursive(middleware, middlewareArray, 0);
+    flattenMiddlewareRecursive(middleware, middlewareArray, currentHeaders, 0);
+
+    _.forEach(currentHeaders, (value, key) => {
+        value.headers[key].isCurrent = true;
+    });
 
     return middlewareArray;
 }
